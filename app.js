45508384a075fbcfcd86e69b3a0c3541f5d8b2c5
crazywhittles <robbiejoe14@gmail.com>
2023-01-22 16:55:41 +0000

    endpoints complete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb, getDb } = require('./db');
+const { ObjectId } = require('mongodb');
 
 const app = express();
 const PORT = 3000;
@@ -28,25 +29,110 @@ app.get('/results', (req, res) => {
         .then(() => {
             res.status(200).json(termScores);
         })
-        .catch(() => {
-            res.status(500).json({ error: 'Could not fetch documents' })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).json({ error: 'Could not fetch documents' });
         })
 })
 
-//see percentage of employees who failed Foundation term
-//app.get()
+//find an employee by their ID
+app.get('/:id', (req, res) => {
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('termScores')
+            .findOne({ _id: ObjectId(req.params.id) })
+            .then(doc => {
+                res.status(200).json(doc)
+            })
+            .catch(() => {
+                res.status(500).json({ error: 'Could not fetch documents' })
+            })
+    } else {
+        res.status(500).json({ error: "Not a valid ID" })
+    }
+})
+
+//see list of employees who failed Foundation term
+app.get('/results/foundation', async (req, res) => {
+
+    try {
+        const termScores = await db.collection("termScores").find({}).toArray();
+        const persons = [];
+        for (const person of termScores) {
+            for (const result of person.results) {
+                if (result.term === "Foundation" && result.score < 37) {
+                    persons.push("Failed Foundation:", person.name);
+                    persons.push(person.results);
+                }
+            }
+        }
+        return res.status(200).json(persons);
+    }
+    catch {
+        res.status(500).json({ error: 'Could not fetch documents' });
+    }
+})
+
+//see list of employees who failed Advanced term
+app.get('/results/advanced', async (req, res) => {
+
+    try {
+        const termScores = await db.collection("termScores").find({}).toArray();
+        const persons = [];
+        for (const person of termScores) {
+            for (const result of person.results) {
+                if (result.term === "Advanced" && result.score < 37) {
+                    persons.push("Failed Advanced:", person.name);
+                    persons.push(person.results);
+                }
+            }
+        }
+        return res.status(200).json(persons);
+    }
+    catch {
+        res.status(500).json({ error: 'Could not fetch documents' });
+    }
+})
 
 //list of employees with total score under 111 marks (failed)
-//app.get()
+app.get('/results/failed', async (req, res) => {
 
-//average score of all employees on Foundation term
-//app.get()
+    try {
+        const termScores = await db.collection("termScores").find({}).toArray();
+        const persons = [];
+        for (let person of termScores) {
+            let sum = 0;
+            for (const result of person.results) {
+                sum += result.score;
+            }
+            if (sum < 111) {
+                persons.push(person.name + " Got a total score of " + sum);
+            }
+        }
+        return res.status(200).json(persons);
+    }
+    catch {
+        res.status(500).json({ error: 'Could not fetch documents' });
+    }
+})
 
 //list of employees with their average score in total
-//app.get()
+app.get('/results/average', async (req, res) => {
 
-//total of employees who failed each term (less than 37 marks for all terms)
-//app.get()
-
-//total of employees who failed in at least one term
-//app.get()
+    try {
+        const termScores = await db.collection("termScores").find({}).toArray();
+        const persons = [];
+        for (let person of termScores) {
+            let sum = 0;
+            for (const result of person.results) {
+                sum += result.score;
+            }
+            let average = Math.floor(sum / 3);
+            persons.push(person.name + " Got an average score of " + average);
+        }
+        return res.status(200).json(persons);
+    }
+    catch {
+        res.status(500).json({ error: 'Could not fetch documents' });
+    }
+})
--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const { MongoClient } = require('mongodb');
 let dbConnection
 
 //results is the db name in Mongodb, and the collection is termScores
+//results are also in results.json so you can see it too
 module.exports = {
     connectToDb: (cb) => {
         MongoClient.connect('mongodb://127.0.0.1:27017/results')
